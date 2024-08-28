d4f8b1f3127fe8ccc4c72aa24a3d73df43af3fe3
Terry Achille <achille.terry@gmail.com>
2024-08-28 16:35:59 -0400

    Create User Endpoint

--- LAMPAPI/CreateUser.php
+++ LAMPAPI/CreateUser.php
@@ -1,4 +1,11 @@
 <?php
+    $inData = getRequestInfo();
+
+    $firstName = "";
+    $lastName = "";
+    $login = "";
+    $password = "";
+
     $conn = new mysqli("localhost", "TheBeast", "Group3Rules", "COP4331");
     if($conn->connect_error)
     {
@@ -6,14 +13,54 @@
     }
     else
     {
-        # Endpoint execution here
+        // No duplicate users
+        $stmt = $conn->prepare("SELECT Login from Users where Login like ?");
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        // Check if rows were return (login already exists)
+        // If user exists return an error
+        if($result->num_rows > 0)
+        {
+            http_response_code(400);
+            returnWithError( "Username already exists" );
+        }
+        // Else insert the user and an ID, then return the ID as a JSON object
+        else
+        {
+            $stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+            $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+            $stmt->execute();
+            
+            $id = $conn->insert_id;
+    
+            $stmt->close();
+            $conn->close();
+
+            http_response_code(200);
+            returnWithInfo(json_encode(array("id"=> $id)));
+        }
     }
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
     function sendResultInfoAsJson( $obj ) 
     {
         header('Content-type: application/json');
         echo $obj;
     }
+
+    function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
     function returnWithError( $err ) 
     {
         $retValue = '{"error":"' . $err . '"}';
