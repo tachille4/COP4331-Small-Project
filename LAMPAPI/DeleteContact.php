c107b87a8be3ee1780212a8fa655827f08ac055b
Terry Achille <achille.terry@gmail.com>
2024-08-31 18:38:37 -0400

    Delete Contact Endpoint

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,22 +1,45 @@
 <?php
-    $conn = new mysqli("localhost", "TheBeast", "Group3Rules", "COP4331");
+    $inData = getRequestInfo();
+
+    $userId = $inData["UserID"];
+    $name = $inData["Name"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
     if($conn->connect_error)
     {
         returnWithError($conn->connect_error);
     }
+    // Delete contact enrty under the name and user ID
     else
     {
-        # Endpoint execution here
+        $stmt = $conn->prepare("DELETE from Contacts where Name=? AND UserID=?");
+        $stmt->bind_param("si", $name, $userId);
+        $stmt->execute();
+
+        $stmt->close();
+        $conn->close();
+
+        http_response_code(200);
+        returnWithError("");
     }
 
+    // Parse incoming JSON request
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    // Set the content header and return the JSON object
     function sendResultInfoAsJson( $obj ) 
     {
-        header('Content-type: application/json');
+        header('Content-Type: application/json');
         echo $obj;
     }
+
+    // Return an error message
     function returnWithError( $err ) 
     {
-        $retValue = '{"error":"' . $err . '"}';
+        $retValue = json_encode(array("error" => $err));
         sendResultInfoAsJson( $retValue );
     }
 ?>
